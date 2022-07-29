0c4110782bc05bb16537db19a155cb45edd01ac2
dimitrileduc <dimitri.leduc87@gmail.com>
2022-07-29 11:06:36 +0200

    beforeRouteLeave alert in edit.vue

--- src/views/event/Edit.vue
+++ src/views/event/Edit.vue
@@ -12,6 +12,22 @@ export default defineComponent({
       type: Object,
       required: true
     }
+  },
+  data: function() {
+    return {
+      unsavedChanges: true // <-- Flag gets set to true if anything
+      //          is changed on the form
+    }
+  },
+  beforeRouteLeave() {
+    if (this.unsavedChanges) {
+      const answer = window.confirm(
+        'Do you really want to leave? You have unsaved changes!'
+      )
+      if (!answer) {
+        return false // <-- Cancels the navigation
+      }
+    }
   }
 })
 </script>
